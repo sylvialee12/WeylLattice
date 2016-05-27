0da9ef9d56d45e1f821832c805b4c24ec7d76891
LI <xlibb@ust.hk>
2016-05-27 13:10:25 +0800

    Python version of program

---
+++ pythonFiles/WeylSuperRadiance.py
@@ -0,0 +1,10 @@
+__author__ = 'xlibb'
+"""
+This is the module of constructing Weyl lattice in superradiance lattice.
+"""
+
+class superradiance:
+    def __init__(self,omega1,omega2,omega):
+        pass
+
+    
